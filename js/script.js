aa8f26820c815b4138ae27d40de6a46da34a6a17
Ismail <ismailhosen8757@gmail.com>
2024-10-07 00:23:42 +0600

    card and 4 button

--- js/script.js
+++ js/script.js
@@ -1,35 +1,65 @@
 const btnAllSection = async() =>{
   const res =await fetch(`https://openapi.programming-hero.com/api/peddy/categories`)
-  const data = await res.json();
-  displayBtn(data.categories)
+  const peta = await res.json();
+  displayBtn(peta.categories)
 }
 const displayBtn = (pets) => {
-  pets.forEach(dat => {
-    // console.log(dat)
-    
+  pets.forEach(pet => {
     const btnSection = document.getElementById('buttonofanimal')
     const newbtn = document.createElement('div')
     newbtn.innerHTML =`
-     <button class="btn hover:bg-btnclr hover:rounded-full">
-     <img class="w-8" src=${dat.category_icon} alt="">
-        ${dat.category}
-       </button>
+    <button id="btn-${pet.id}" onclick="categoryName('${pet?.category || 'defaultCategory'}')" class="btn hover:bg-btnclr hover:rounded-full">
+    <img class="w-8" src="${pet.category_icon}" alt="">
+    ${pet.category}
+    </button>
     `
     btnSection.append(newbtn)
   
   });
 }
+
+const categoryName = (categoryname) => {
+  document.getElementById('loading').classList.remove('hidden')
+  document.getElementById('card-container').classList.add('hidden')
+  document.getElementById('aa').classList.add('hidden')
+  setTimeout(() => {
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryname}`)
+    .then((res) => res.json())
+    .then((peta) =>
+      displayAllCard(peta.data))
+
+    .catch((error) => console.log(error)) 
+    document.getElementById('loading').classList.add('hidden')
+    document.getElementById('card-container').classList.remove('hidden')
+    document.getElementById('aa').classList.remove('hidden')
+  },1500);
+}
+
+
 const showAnimal = async () => {
   const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
-  const data = await response.json();
-  displayAllCard(data.pets);  // Pass the 'pets' array to displayAllCard
+  const peta = await response.json();
+  displayAllCard(peta.pets);  // Pass the 'pets' array to displayAllCard
 }
 
 const displayAllCard = (cards) => {
-
+ 
+const cardCcontainer = document.getElementById('card-container')
+cardCcontainer.innerHTML =''
+if(cards.length == 0){
+  cardCcontainer.classList.remove('grid')
+  cardCcontainer.innerHTML =`
+  <div class=" min-h-[300px] flex flex-col gap-5 justify-center items-center">
+  <img src="assest/error.webp" alt="">
+  <h2 class="font-bold text-xl text-center">No Information Available</h2>
+  <p class="text-[gray] w-2/3 text-center">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a.</p>
+  </div>
+  `
+}else{
+  cardCcontainer.classList.add('grid')
+}
   cards.forEach((card) => {
-    const {breed, category, date_of_birth,price, image, gender, vaccinated_status,pet_name}= card;
-     const cardCcontainer = document.getElementById('card-container')
+    const {breed,petId,category,pete_of_birth,price,image,gender,vaccinated_status,pet_name,}= card;
        const div = document.createElement("div")    
        div.innerHTML =`
        <div class="card  bg-base-100  border border-[#0E7A8126] ">
@@ -42,7 +72,7 @@ const displayAllCard = (cards) => {
              <i class="fa-solid fa-cubes mr-2"></i> Breed:${breed}
            </p>
            <p class="text-start text-base font-normal text-[#131313B3]">
-             <i class="fa-solid fa-calendar-days mr-2"></i> Birth: ${date_of_birth?date_of_birth: 'N/A'}
+             <i class="fa-solid fa-calendar-days mr-2"></i> Birth: ${pete_of_birth?pete_of_birth: 'N/A'}
            </p>
            <p class="text-start text-base font-normal text-[#131313B3]">
              <i class="fa-solid fa-venus mr-2"></i> Gender: ${gender?gender: "N/A"}
@@ -52,19 +82,68 @@ const displayAllCard = (cards) => {
            </p>
            <hr class="w-full" />
            <div class="card-actions grid grid-cols-3 gap-2 mt-2">
-             <button class="btn bg-white border-[#0E7A8126] outline-1">
+             <button onclick="createNewPart('${image}')" class="btn bg-white border-[#0E7A8126] outline-1">
                <i class="fa-regular fa-thumbs-up text-2xl "></i>
              </button>
              <button class="btn border-[#0E7A8126]  bg-white text-[18px] font-bold text-[#0E7A81] p-1">Adopt</button>
-             <button class="btn border-[#0E7A8126] bg-white text-[18px] font-bold text-[#0E7A81] p-1">Details</button>
+             <button onclick="createModal('${petId}')" class="btn border-[#0E7A8126] bg-white text-[18px] font-bold text-[#0E7A81] p-1">Details</button>
              <div></div>
            </div>
          </div>
        </div>
        `
        cardCcontainer.appendChild(div)
-
   });
+}
+const createNewPart = (imagee) => {
+  // console.log(id)
+  const createNEWSection = document.getElementById('createNEWSection')
+  const div = document.createElement('div')
+  div.innerHTML=`
+  <img class="rounded-md" src="${imagee}" alt="" />
+  ` 
+  createNEWSection.append(div)
+}
+const createModal = async(ID)=>{
+      const res = await fetch (`https://openapi.programming-hero.com/api/peddy/pet/${ID}`)
+      const data = await res.json()
+      MODAL(data.petData)
+}
+const MODAL = (details) =>{
+  const {breed,petId,category,pete_of_birth,price,image,gender,pet_details,vaccinated_status,pet_name,}= details;
+  const modalContainer = document.getElementById('modal-Container')
+  modalContainer.innerHTML=`
+  <dialog id="my_modal_1" class="modal">
+  <div class="modal-box">
+   <figure class="p-2 ">
+           <img class="w-full rounded-lg" src=${image}  />
+         </figure>
+    <div class="p-2 ">
+      <h3 class="text-[20px] font-bold text-[#131313]">${pet_name?pet_name: 'N/A'}      
+      <p class="text-start text-base font-normal text-[#131313B3]">
+        <i class="fa-solid fa-cubes mr-2"></i> Breed:${breed}
+      </p>
+      <p class="text-start text-base font-normal text-[#131313B3]">
+        <i class="fa-solid fa-calendar-days mr-2"></i> Birth: ${pete_of_birth?pete_of_birth: 'N/A'}
+      </p>
+      <p class="text-start text-base font-normal text-[#131313B3]">
+        <i class="fa-solid fa-venus mr-2"></i> Gender: ${gender?gender: "N/A"}
+      </p>
+      <p class="text-start text-base font-normal text-[#131313B3]">
+        <i class="fa-solid fa-dollar-sign mr-2"></i> Price: ${price?price: "N/A"}
+      </p>
+      <hr/>
+      <h1 class="font-bold mt-3">Details Information</h1>
+      <p>${pet_details}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <div class="w-full"><button class="btn ">Close</button></div>
+      </form>
+    </div>
+  </div>
+  </dialog>
+  `
+my_modal_1.showModal()
 }
 
 showAnimal()
